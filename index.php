3897b566a8394411ca2a9ba1f274934cf1b8456a
F4uzan <f4uzan@this.is.f4>
2017-01-31 10:48:29 +0700

    Move CSS to their own separate folder
    Following one of the change a7b64f38a2810836356481dfb0226cabeaabdb9c, also move CSS to their separate folder

--- index.php
+++ index.php
@@ -23,8 +23,8 @@
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="material.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/material.min.css">
+    <link rel="stylesheet" href="css/styles.css">
     <style>
     #view-source {
       position: fixed;
