a7b64f38a2810836356481dfb0226cabeaabdb9c
F4uzan <f4uzan@this.is.f4>
2017-01-31 10:44:22 +0700

    Changes to PHP scripts
    * Move these scripts to their own separate folder
    * Fix plugins path after moving the scripts
    * Add a shared common function that can be included in other script
    * Add an offline copy for Material Design Lite's JS

--- index.php
+++ index.php
@@ -1,26 +1,4 @@
-<?php
-/**
- * function xml2array
- *
- * This function is part of the PHP manual.
- *
- * The PHP manual text and comments are covered by the Creative Commons 
- * Attribution 3.0 License, copyright (c) the PHP Documentation Group
- *
- * @author  k dot antczak at livedata dot pl
- * @date    2011-04-22 06:08 UTC
- * @link    http://www.php.net/manual/en/ref.simplexml.php#103617
- * @license http://www.php.net/license/index.php#doc-lic
- * @license http://creativecommons.org/licenses/by/3.0/
- * @license CC-BY-3.0 <http://spdx.org/licenses/CC-BY-3.0>
- */
-function xml2array ( $xmlObject, $out = array () ) {
-    foreach ( (array) $xmlObject as $index => $node )
-        $out[$index] = ( is_object ( $node ) ) ? xml2array ( $node ) : $node;
-
-    return $out;
-}
-?>
+<?php include 'php/common.php'; ?>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -83,7 +61,7 @@ function xml2array ( $xmlObject, $out = array () ) {
           <section class="section--center mini-card mdl-grid mdl-grid--no-spacing mdl-shadow--6dp">
             <div class="mdl-card mdl-cell mini-card mdl-cell--12-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
               <div class="mdl-card__supporting-text">
-				<form action="action.php" method="GET" id="search-field">
+				<form action="php/action.php" method="GET" id="search-field">
 				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 					<input class="mdl-textfield__input" type="text" name="search" id="search">
 					<label class="mdl-textfield__label" for="search">Search</label>
@@ -119,5 +97,5 @@ function xml2array ( $xmlObject, $out = array () ) {
         </div>
       </main>
     </div>
-    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <script src="js/material.min.js"></script>
 </body>
--- php/common.php
+++ php/common.php
@@ -1,4 +1,6 @@
 <?php
+// Common shared function
+
 /**
  * function xml2array
  *
@@ -20,25 +22,4 @@ function xml2array ( $xmlObject, $out = array () ) {
 
     return $out;
 }
-
-	$txt = urlencode($_GET['search']);
-	$engine = $_GET['engine'];
-	
-	$plugin_dir = glob('plugins/*.xml');
-					
-		if (is_array($plugin_dir)) {
-			foreach($plugin_dir as $filename) {
-			$raw_plugin = simplexml_load_file($filename) or die("Cannot create object");
-			$plugin_load = xml2array($raw_plugin);
-							
-			$id = $plugin_load['id'];
-			$icon = $plugin_load['icon'];
-			$icon_format = $plugin_load['icon_format'];
-			$search_uri = $plugin_load['search_uri'];
-			
-			if ($engine == $id) {
-				header("location: $search_uri$txt");
-			}
-		}
-	}
 ?>
